00c855bb83147be96d22607a73852ab279389224
Apoorva <apoorvabhide017@gmail.com>
2023-04-25 13:10:44 +0530

    file is updated

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ class App{
 	public static void main(String[] args){
 		System.out.println("Hey! There I am using Notepad++");
 		System.out.println("Hey! there i am using whatsapp");
+		System.out.println("Java"+10+20);
 	}
 }
